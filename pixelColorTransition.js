d9b1e7a2175a7c7b13e2591a503927958045da65
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-01 20:32:53 +0300

    extracting input data on submit

--- pixelColorTransition.js
+++ pixelColorTransition.js
@@ -4,3 +4,15 @@
 const offscreenCanvas = new OffscreenCanvas(1, 1);
 const offscreenContext = offscreenCanvas.getContext('2d', { wilReadFrequently: true });
 const imageForm = document.getElementById('image-input-form');
+// extract input data
+imageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const formData = Object.fromEntries(new FormData(e.currentTarget));
+  const inputImageFile = formData['image-file'];
+  const pixelSize = Number(formData['pixel-size']);
+  
+  // if it's not an image file (type === image/*) don't continue
+  const imageInTheBeginningRegex = /^image/;
+  if (!imageInTheBeginningRegex.test(inputImageFile.type)) throw new Error('file should be an image file');
+})
