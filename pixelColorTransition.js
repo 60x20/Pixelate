f25d75bc3e8afad1dd929482208b670ce6561a5b
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-01 22:17:43 +0300

    comment edited

--- pixelColorTransition.js
+++ pixelColorTransition.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// width and height will be set programmatically
+// width and height will be set programmatically (if set bigger than necessary, performance issues will arise)
 const offscreenCanvas = new OffscreenCanvas(1, 1);
 const offscreenContext = offscreenCanvas.getContext('2d', { wilReadFrequently: true });
 const imageForm = document.getElementById('image-input-form');
